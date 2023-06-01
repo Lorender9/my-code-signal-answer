eda179b1690f7170fa970865e1f2573ffccfa119
Francisco Javier Reyes Castillo <107876442+Lorender9@users.noreply.github.com>
2023-06-01 10:42:47 -0400

    Update edge-of-the-ocean-1.py

--- arcade-mode/python/edge-of-the-ocean/edge-of-the-ocean-1.py
+++ arcade-mode/python/edge-of-the-ocean/edge-of-the-ocean-1.py
@@ -1,1 +1,7 @@
-
+def solution(inputArray):
+    max_product = float("-inf")
+    for aux in range(len(inputArray)-1):
+        product = inputArray[aux]*inputArray[aux+1]
+        if(product >= max_product):
+            max_product = product
+    return max_product
